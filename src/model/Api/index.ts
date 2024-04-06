41c8e6068e7ee25a3906e779cbcbbed264d93336
ghalielouarzazi@hotmail.com <ghalielouarzazi@hotmail.com>
2024-04-06 15:51:36 +0200

    lint

--- src/model/Api/index.ts
+++ src/model/Api/index.ts
@@ -129,16 +129,18 @@ export class Api {
       // Inject avail specific types
       const endpoint =
         NetworkList[this.network].endpoints.rpcEndpoints[this.#rpcEndpoint];
-      const customTypes = (endpoint.includes('goldberg') || endpoint === "wss://rpc-testnet.avail.tools/ws")
-        ? {
-          types: goldbergTypes,
-          rpc: goldbergRpc,
-        }
-        : {
-          types: availTypes,
-          rpc: availRpc,
-          signedExtensions: availSignedExtensions,
-        };
+      const customTypes =
+        endpoint.includes('goldberg') ||
+        endpoint === 'wss://rpc-testnet.avail.tools/ws'
+          ? {
+              types: goldbergTypes,
+              rpc: goldbergRpc,
+            }
+          : {
+              types: availTypes,
+              rpc: availRpc,
+              signedExtensions: availSignedExtensions,
+            };
 
       // Initialise api.
       this.#api = new ApiPromise({
